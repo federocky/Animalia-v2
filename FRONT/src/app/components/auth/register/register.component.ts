3b2577102d4fb05970f6e76e388bc975a08273cb
Federico Andrés Jácome Castañeda <64231187+federocky@users.noreply.github.com>
2021-03-28 21:59:15 +0200

    validacion de coincidir contraseña al registrarse

--- FRONT/src/app/components/auth/register/register.component.ts
+++ FRONT/src/app/components/auth/register/register.component.ts
@@ -1,6 +1,6 @@
 import { AuthService } from './../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
@@ -24,7 +24,8 @@ export class RegisterComponent implements OnInit {
   repeatPassword:  FormControl;
 
   constructor( private _authService: AuthService,
-                private router: Router
+                private router: Router,
+                private fb: FormBuilder
     ) { 
     this.createForm();
   }
@@ -34,20 +35,38 @@ export class RegisterComponent implements OnInit {
 
   createForm(): void {
 
-    this.myForm = new FormGroup({
+    this.myForm = this.fb.group({
       name: new FormControl('', Validators.required), 
       surname: new FormControl('', Validators.required),
-      phone: new FormControl(''),
+      phone: new FormControl('', [Validators.required, Validators.pattern('[0-9]{9}')]),
       email: new FormControl('', [Validators.required, Validators.pattern('[^ @]*@[^ @]+')]),
       password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-      repeatPassword: new FormControl('', Validators.required)
+      repeatPassword: new FormControl('',)
       //FIXME: comprobar que la contraseña no se pueda repetir, ver fernando formularios.
+  },{
+    validators: this.passwordsIguales('password', 'repeatPassword')
   });
 
   this.setFormControlsVariables();
 
   }
 
+  passwordsIguales( pass1Name: string, pass2Name: string){
+    
+    return( formGroup: FormGroup ) => {
+      const pass1Control = formGroup.controls[pass1Name];
+      const pass2Control = formGroup.controls[pass2Name];
+
+      if(pass1Control.value === pass2Control.value) {
+        pass2Control.setErrors(null);
+      }else {
+        pass2Control.setErrors({noEsIgual: true});
+      }
+    }
+  }
+
+
+  
   setFormControlsVariables(): void {
 
     const fgName: FormGroup = this.myForm.controls.name as FormGroup;
@@ -80,7 +99,8 @@ export class RegisterComponent implements OnInit {
         email: this.email.value,
         phone: this.phone.value,
         password: this.password.value
-      }
+      } 
+
 
       this._authService.signUp(user)
         .subscribe( res => {
